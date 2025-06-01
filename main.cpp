ecb50e0e859ff34662c1fcc10754bae338f6c5ff
Suhe1l <astudiobyad@gmail.com>
2025-06-01 16:36:02 +0300

    Create a BMI calculator application that reads the user’s weight in pounds and height in inches (or, if you prefer, the user’s weight in kilograms and height in meters), then calculates and displays the user’s body mass index. Also, the application should display the information from the Department of Health and Human Services/National Institutes of Health so the user can evaluate his/her BMI.

--- main.cpp
+++ main.cpp
@@ -1,22 +1,43 @@
-// Trick math question testing the division and modulus operator
+// Simple BMI calculator
 #include <iostream>
+#include <iomanip>
+using namespace std;
 
 int main() {
-    int number;
-    
-    std::cout << "Enter a five-digit integer: ";
-    std::cin >> number;
+    double weight, height, bmi;
+    int choice;
 
-    // Extract digits using division and modulus
-    int first = number / 10000;
-    int second = (number / 1000) % 10;
-    int third = (number / 100) % 10;
-    int fourth = (number / 10) % 10;
-    int fifth = number % 10;
+    cout << "Select measurement system:\n";
+    cout << "1. Metric (kg/m)\n";
+    cout << "2. Imperial (lbs/in)\n";
+    cout << "Enter choice (1 or 2): ";
+    cin >> choice;
 
-    // Print separated digits
-    std::cout << first << "   " << second << "   " << third << "   " 
-              << fourth << "   " << fifth << std::endl;
+    if (choice == 1) {
+        cout << "Enter weight (kg): ";
+        cin >> weight;
+        cout << "Enter height (meters): ";
+        cin >> height;
+        bmi = weight / (height * height);
+    } else if (choice == 2) {
+        cout << "Enter weight (lbs): ";
+        cin >> weight;
+        cout << "Enter height (inches): ";
+        cin >> height;
+        bmi = (weight * 703) / (height * height);
+    } else {
+        cout << "Invalid selection!";
+        return 1;
+    }
+
+    cout << fixed << setprecision(2);
+    cout << "\nYour BMI is: " << bmi << "\n";
+
+    cout << "BMI Categories:\n";
+    cout << "Underweight: less than 18.5\n";
+    cout << "Normal: between 18.5 and 24.9\n";
+    cout << "Overweight: bewteen 25 and 29.9\n";
+    cout << "Obese: 30 or greater\n";
 
     return 0;
 }
